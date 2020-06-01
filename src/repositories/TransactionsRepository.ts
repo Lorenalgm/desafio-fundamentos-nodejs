975757c1baa3a91eb7d4bf1a16b247f5cf58bc7a
Lorenalgm <lorena@proesc.com>
2020-05-31 23:31:09 -0300

    MAdded verification test

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,15 +1,15 @@
 import Transaction from '../models/Transaction';
 
-interface CreateTransactionDTO {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-}
-
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
